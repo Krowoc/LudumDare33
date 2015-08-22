49b5aec847ee51be7290467133d2f2fbfab2644c
Adam Silcott <adsilcott@gmail.com>
2015-08-22 15:47:07 -0400

    fixed scale and physics, added spawner

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Mario mario = other.GetComponent<Mario>();
+
+		mario.Jump ();
+		Debug.Log (mario);
+
+		if(mario != null)
+			mario.Jump ();
+	}
+}
