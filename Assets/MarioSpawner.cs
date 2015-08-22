09f60b8d1948a55ade2f5b066f2b15750a263372
Jesse Smith <jesse.smth09@gmail.com>
2015-08-22 11:34:21 -0400

    Mario Spawner added

---
+++ Assets/MarioSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioSpawner : MonoBehaviour {
+
+
+	public GameObject Mario;
+	public float delayInSeconds;
+	public bool isActive = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		StartCoroutine ("Spawn");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator Spawn()
+	{
+		while (isActive) 
+		{
+			GameObject mario = Instantiate(Resources.Load(Mario.name, typeof(GameObject))) as GameObject;
+			mario.transform.position = transform.position;
+
+			yield return new WaitForSeconds(delayInSeconds);
+
+		}
+	}
+}
