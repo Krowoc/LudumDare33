e641874fe3c64f400b3827102677a7e8714c17d6
Jesse Smith <jesse.smth09@gmail.com>
2015-08-22 12:22:00 -0400

    More marios spawns at certain time intervals

--- Assets/MarioSpawner.cs
+++ Assets/MarioSpawner.cs
@@ -1,12 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MarioSpawner : MonoBehaviour {
 
 
 	public GameObject Mario;
-	public float delayInSeconds;
-	public bool isActive = true;
+	public float      delayInSeconds;
+	public bool       isActive= true;
+	public float      decreaseInTime;
+	public float      decreaseDelay;
+	public float      increaseInTime;
 
 	// Use this for initialization
 	void Start () 
@@ -19,6 +22,13 @@ public class MarioSpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		decreaseInTime -= Time.deltaTime;
+
+		if (decreaseInTime <= 0) 
+			{
+			    decreaseInTime += increaseInTime;
+				delayInSeconds -= decreaseDelay;
+			}
 	
 	}
 
@@ -28,6 +38,8 @@ public class MarioSpawner : MonoBehaviour {
 		{
 			GameObject mario = Instantiate(Resources.Load(Mario.name, typeof(GameObject))) as GameObject;
 			mario.transform.position = transform.position;
+			body_Mario.velocity = (Vector2.right * 10);
+
 
 			yield return new WaitForSeconds(delayInSeconds);
 
