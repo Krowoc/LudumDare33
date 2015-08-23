41c23e0d95495c3e8bacca4b5c1023b248ce256c
Jesse Smith <jesse.smth09@gmail.com>
2015-08-23 12:36:11 -0400

    revised playerPlant

--- Assets/PlayerPlant.cs
+++ Assets/PlayerPlant.cs
@@ -3,40 +3,53 @@ using System.Collections;
 
 public class PlayerPlant : MonoBehaviour {
 	
-	public Rigidbody2D plant;
+	public GameObject  plant;
 	public Vector3     startpos;
 	public Vector3     endpos;
 	public float       moveDistance;
 	public float       lerpTime;
 	public float       currentLerpTime;
+	public bool        atBase = true;
+	                   GameObject  baseCheck;
 
 	// Use this for initialization
 	void Start () 
 	{
-		startpos = transform.position; 
-		endpos = transform.position + transform.up * moveDistance;
+//		baseCheck = transform.FindChild ("baseCheck").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
-		if (Input.GetButtonDown("Fire1")) 
+		if (Input.GetButtonDown ("Fire1")) 
+		{   
+//			atBase = GroundTest();
+//			if(atBase){
+			 	currentLerpTime = 0f;
+				plant = GameObject.FindGameObjectWithTag("player1");
+				Attack ();
+//			  }
+		} 
+		else if (Input.GetButtonDown ("Fire2")) 
 		{
 			currentLerpTime = 0f;
-
-			currentLerpTime += Time.deltaTime;
-			if (currentLerpTime > lerpTime) 
-			{
-				currentLerpTime = lerpTime;
-			}
-
-			float perc = currentLerpTime / lerpTime;
-			transform.position = Vector3.Lerp(startpos, endpos, perc);
-			transform.position = Vector3.Lerp(endpos, startpos, perc);
-		 }
+			plant = GameObject.FindGameObjectWithTag("player2");
+			Attack ();
+		} 
+		else if (Input.GetButtonDown ("Fire3")) 
+		{
+			currentLerpTime = 0f;
+			plant = GameObject.FindGameObjectWithTag("player3");
+			Attack ();
+		}
 
 	}
+
+//	bool GroundTest()
+//	{
+//		return Physics2D.Raycast (baseCheck.transform.position, Vector2.down, 0.1f);
+//	}
+
 	void OnCollisionEnter2D(Collision2D coll) 
 	{
 
@@ -46,6 +59,24 @@ public class PlayerPlant : MonoBehaviour {
 			Destroy(coll.gameObject);
 		} 
 	}
+
+	void Attack()
+	{
+		startpos = plant.transform.position; 
+		endpos = plant.transform.position + transform.up * moveDistance;
+		
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) 
+		{
+			currentLerpTime = lerpTime;
+		}
+		
+		float perc = currentLerpTime / lerpTime;
+		plant.transform.position = Vector3.Lerp(startpos, endpos, perc);
+		plant.transform.position = Vector3.Lerp(endpos, startpos, perc);
+	}
+
+
 
 
 
