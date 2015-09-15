61cec8317b738aa395f00f6efff80aa1111d9c34
Ian Lane <islane@rocketmail.com>
2015-09-14 23:14:10 -0400

    Edits to the main menu scripts, added buttons and variables related to HighScoreControl.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+	// Variables
+	GameObject popUp1;
+	Vector3 currentPosition;
+	public Text ScoreDisplay;
+	public int ScoreDisplayInt;
+	public Text NameDisplayText;
+	public string NameDisplay;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		popUp1 = GameObject.Find ("EntryPanel");
+		currentPosition = popUp1.transform.localPosition;
+		popUp1.transform.localPosition = new Vector3 (1000, 1000);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -33,6 +45,33 @@ public class MainMenu : MonoBehaviour {
 	public void OnBackButton()
 	{
 		Application.LoadLevel ("TitleScreen");
+	}
+
+	public void OnUploadButton()
+	{
+		GameObject.Find ("UploadButton").SetActive (false);
+
+		popUp1.transform.localPosition = currentPosition;
+	}
+
+	public static int ToInt32(string value)
+	{
+		if (value == null)    
+			return 0;
+		
+		return System.Int32.Parse(value, System.Globalization.CultureInfo.CurrentCulture);
+	}
+
+	public void OnSubmitButton()
+	{
+		ScoreDisplay = GameObject.Find ("Score").GetComponent<Text>();
+		ScoreDisplayInt = ToInt32 (ScoreDisplay.text);
+		NameDisplayText = GameObject.Find ("InputText").GetComponent<Text> ();
+		NameDisplay = NameDisplayText.ToString();
+
+		Debug.Log (NameDisplay.ToString());
+		GameObject.Find ("HighScoreText").GetComponent<HighScoreControl> ().PostScores(NameDisplay, ScoreDisplayInt);
+		//Application.LoadLevel ("Credits");
 	}
 
 	public void OnExitButton()
